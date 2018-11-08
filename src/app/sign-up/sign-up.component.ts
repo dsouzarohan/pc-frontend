39092ac0f35349f6d08aa6de1a1625ce16bf76f9
therealdexter <dsouzarohan239@gmail.com>
2018-11-08 22:47:47 +0530

    Added two steps to the sign up component

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormControl, FormGroup, NgForm, Validators} from '@angular/forms';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
 
 @Component({
@@ -14,23 +14,38 @@ export class SignUpComponent implements OnInit {
   divisions of the entire sign up form
    */
 
+  @ViewChild('confirmPassword') confirmPassword: ElementRef;
+
   private signUpForm: FormGroup;
 
   private typeFormGroup: FormGroup;
   private personalFormGroup: FormGroup;
   private contactFormGroup: FormGroup;
-  private passwordFormGroup: FormGroup;
-
-  private isEditable = true;
+  private credentialFormGroup: FormGroup;
 
   private types = [
     'Student',
     'Teacher'
   ];
 
+  private genders = [
+    'Male',
+    'Female',
+    'Other'
+  ];
+
+  private maxDate: Date;
+
   constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit() {
+
+    this.maxDate = new Date();
+    let today: Date = new Date();
+
+    this.maxDate.setDate(today.getDate()-1);
+
+    //Form Groups
 
     this.typeFormGroup = this.formBuilder.group({
       'type': ['', Validators.required]
@@ -51,30 +66,70 @@ export class SignUpComponent implements OnInit {
       ]],
       'dateOfBirth': ['',
       [
-        Validators.required,
-        this.validateBirthdate
-      ]]
-      // 'gender': ['',[
-      //   Validators.required
-      // ]]
+        Validators.required
+      ]],
+      'gender': ['',
+        [
+          Validators.required
+        ]]
     });
+
+    this.contactFormGroup = this.formBuilder.group({
+      mobileNumber: ['',[
+        Validators.required,
+        Validators.pattern(/^(\+\d{1,3}[- ]?)?\d{10}$/)
+      ]],
+      alternateMobileNumber: ['',Validators.pattern(/^(\+\d{1,3}[- ]?)?\d{10}$/)],
+      email: ['',[
+        Validators.required,
+        Validators.email
+      ]]
+    })
+
+    this.credentialFormGroup = this.formBuilder.group({
+      email: ['', [
+        Validators.required,
+        Validators.email
+      ]],
+      password: ['',[
+        Validators.required,
+        Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,15}$/),
+        this.passwordMatch.bind(this)
+      ]],
+      confirmPassword: ['',[
+        Validators.required,
+        Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,15}$/),
+        this.passwordMatch.bind(this)
+      ]]
+    });
+
+    //Main sign up form
 
     this.signUpForm = new FormGroup({
       type: this.typeFormGroup,
-      personal: this.personalFormGroup
+      personal: this.personalFormGroup,
+      contact: this.contactFormGroup,
+      credentials: this.credentialFormGroup
     });
 
   }
 
-  onSubmit(){
-    console.log(this.signUpForm);
+  passwordMatch(passwordControl: FormControl): {[key: string]: boolean}{
+
+    //todo: fix the error of cannot read 'get' caused by the two lines below and complete the form
+
+    let password = this.credentialFormGroup.get('password').value;
+    // let confirmPassword = this.credentialFormGroup.get('confirmPassword').value;
+
+    // if(password === confirmPassword){
+    //   return null;
+    // }
+
+    return {'Passwords do not match': true};
   }
 
-  private validateBirthdate(dobControl: FormControl) : {[s: string]: boolean} {
-
-    //todo: Check out the datepicker docs and complete the whole form
-
-    return null;
+  onSubmit(){
+    console.log(this.credentialFormGroup);
   }
 
 }
